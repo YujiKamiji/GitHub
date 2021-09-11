4f0bed9242cf797bf97e693c037b32e69cc1a955
YujiKamiji <yujikamiji@gmail.com>
2021-09-11 11:23:40 -0300

    changed script 3

--- Assets/Scripts/Script3.cs
+++ Assets/Scripts/Script3.cs
@@ -7,7 +7,7 @@ public class Script3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var x = 0;
     }
 
     // Update is called once per frame
